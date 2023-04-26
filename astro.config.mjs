c1ab29c5fe9210537f09e491a75762164ed6ac28
Wilmer <wilmer.r2004@gmail.com>
2023-04-26 14:42:14 -0500

    preparando para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: "https://wilmerHRS.github.io",
+  base: "/portfolio",
 });
